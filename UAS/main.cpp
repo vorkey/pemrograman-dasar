9f20405583296cc250fcf67f41cf06da01f3621d
vorkey <ambadayaakun@gmail.com>
2024-12-10 08:18:33 +0800

    backup

---
+++ UAS/main.cpp
@@ -0,0 +1,22 @@
+// Ambadaya Gaman - C020324001 - Elektronika 1A
+// Jhonly Ardianto - C020324008 - Elektronika 1B
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int MAX_BARANG = 50; // Maksimum jumlah barang
+
+// Fungsi untuk menampilkan menu
+void tampilkanMenu() {
+  cout << "\n=========== MENU ==========\n";
+  cout << "1. Tambah Data Barang\n";
+  cout << "2. Tampilkan Semua Data Barang\n";
+  cout << "3. Cari Barang Berdasarkan Nama\n";
+  cout << "4. Hitung Total Stok Barang\n";
+  cout << "5. Hitung Total Nilai Gudang\n";
+  cout << "6. Keluar\n";
+  cout << "Pilih Menu: \n";
+}
+
+int main() { return 0; }

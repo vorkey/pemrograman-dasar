9e12520e2194b49b7496d8c7c5f8ba756680f316
vorkey <ambadayaakun@gmail.com>
2024-12-10 08:37:57 +0800

    backup

--- UAS/main.cpp
+++ UAS/main.cpp
@@ -19,4 +19,49 @@ void tampilkanMenu() {
   cout << "Pilih Menu: \n";
 }
 
-int main() { return 0; }
+int main() {
+  string namaBarang[MAX_BARANG];
+  int stokBarang[MAX_BARANG];
+  float hargaBarang[MAX_BARANG];
+  char kategoriBarang[MAX_BARANG]; // Kategori: A = Elektronik, B = Furniture, C
+                                   // = Alat Tulis
+  int jumlahBarang = 0;
+  int pilihan;
+
+  do {
+    tampilkanMenu();
+    cin >> pilihan;
+    switch (pilihan) {
+    case 1: // Tambah Data Barang
+      if (jumlahBarang < MAX_BARANG) {
+        cout << "Masukkan nama barang : ";
+        cin.ignore();
+        getline(cin, namaBarang[jumlahBarang]);
+        cout << "Masukkan jumlah stok barang: ";
+        cin >> stokBarang[jumlahBarang];
+        cout << "Masukkan harga barang (per unit): ";
+        cin >> hargaBarang[jumlahBarang];
+        cout << "Masukkan kategori barang (A = Elektronik, B = Furniture, C = "
+                "Alat Tulis): ";
+        cin >> kategoriBarang[jumlahBarang];
+        jumlahBarang++;
+        cout << "Data barang berhasil ditambahkan!\n";
+      } else {
+        cout << "Gudang penuh, tidak dapat menambahkan barang lagi.\n";
+      }
+      break;
+    case 2: // Tampilkan Semua Data Barang
+      if (jumlahBarang == 0) {
+        cout << "Belum ada data barang di gudang.\n";
+      } else {
+        cout << "\n=== Data Barang ===\n";
+        for (int i = 0; i < jumlahBarang; i++) {
+          cout << i + 1 << ". " << namaBarang[i]
+               << " - Stok: " << stokBarang[i];
+        }
+      }
+    }
+  } while ();
+
+  return 0;
+}
